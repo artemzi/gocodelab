66a23c341aa4978b7cb1426dc6e4da53368fb600
Artem Zinoviev <arzinoviev@gmail.com>
2018-07-23 22:43:18 +0300

    Add storage

---
+++ storage/storage.go
@@ -0,0 +1,95 @@
+package storage
+
+import (
+	"errors"
+	"math"
+)
+
+type (
+	// Location used for storing driver's location
+	Location struct {
+		Lat float64
+		Lon float64
+	}
+	// Driver model to store driver data
+	Driver struct {
+		ID           int
+		LastLocation Location
+	}
+)
+
+type DriverStorage struct {
+	drivers map[int]*Driver
+}
+
+// New creates new instance of DriverStorage
+func New() *DriverStorage {
+	d := &DriverStorage{}
+	d.drivers = make(map[int]*Driver)
+	return d
+}
+
+// Set sets driver to storage by key
+func (d *DriverStorage) Set(key int, driver *Driver) {
+	d.drivers[key] = driver
+}
+
+// Delete removes driver from storage by key
+func (d *DriverStorage) Delete(key int) error {
+	_, ok := d.drivers[key]
+	if !ok {
+		return errors.New("Driver does not exist")
+	}
+	delete(d.drivers, key)
+	return nil
+}
+
+// Get gets driver from storage and an error if nothing found
+func (d *DriverStorage) Get(key int) (*Driver, error) {
+	driver, ok := d.drivers[key]
+	if !ok {
+		return nil, errors.New("Driver does not exist")
+	}
+	return driver, nil
+}
+
+// Nearest returns nearest drivers by locaion
+// https://github.com/artemzi/gocodelabru/blob/master/step07/README.md#%D0%A0%D0%B5%D0%B0%D0%BB%D0%B8%D0%B7%D1%83%D0%B5%D0%BC-nearest-%D0%BC%D0%B5%D1%82%D0%BE%D0%B4
+func (d *DriverStorage) Nearest(radius, lat, lon float64) ([]*Driver, error) {
+	var result []*Driver
+	for _, driver := range d.drivers {
+		distance := Distance(lat, lon, driver.LastLocation.Lat, driver.LastLocation.Lon)
+		if distance <= radius {
+			result = append(result, driver)
+		}
+	}
+	return result, nil
+}
+
+// haversin(θ) function
+func hsin(theta float64) float64 {
+	return math.Pow(math.Sin(theta/2), 2)
+}
+
+// Distance function returns the distance (in meters) between two points of
+//     a given longitude and latitude relatively accurately (using a spherical
+//     approximation of the Earth) through the Haversin Distance Formula for
+//     great arc distance on a sphere with accuracy for small distances
+//
+// point coordinates are supplied in degrees and converted into rad. in the func
+//
+// distance returned is METERS!!!!!!
+// http://en.wikipedia.org/wiki/Haversine_formula
+func Distance(lat1, lon1, lat2, lon2 float64) float64 {
+	// convert to radians
+	// must cast radius as float to multiply later
+	var la1, lo1, la2, lo2, r float64
+	la1 = lat1 * math.Pi / 180
+	lo1 = lon1 * math.Pi / 180
+	la2 = lat2 * math.Pi / 180
+	lo2 = lon2 * math.Pi / 180
+	r = 6378100 // Earth radius in METERS
+	// calculate
+	h := hsin(la2-la1) + math.Cos(la1)*math.Cos(la2)*hsin(lo2-lo1)
+	return 2 * r * math.Asin(math.Sqrt(h))
+}
