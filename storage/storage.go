8ed399757730a342f093e7783f0ef05d49830290
Artem Zinoviev <arzinoviev@gmail.com>
2018-07-24 22:14:27 +0300

    Refactor storage

--- storage/storage.go
+++ storage/storage.go
@@ -2,7 +2,10 @@ package storage
 
 import (
 	"errors"
+	"sync"
+	"time"
 
+	"github.com/artemzi/gocodelab/storage/lru"
 	"github.com/dhconnelly/rtreego"
 )
 
@@ -16,12 +19,23 @@ type (
 	Driver struct {
 		ID           int
 		LastLocation Location
+		Expiration   int64
+		Locations    *lru.LRU
+	}
+	DriverStorage struct {
+		mu        *sync.RWMutex // для синхронизации
+		drivers   map[int]*Driver
+		locations *rtreego.Rtree
+		lruSize   int // для того, чтобы инициализировать хранилище по каждому водителю
 	}
 )
 
-type DriverStorage struct {
-	drivers   map[int]*Driver
-	locations *rtreego.Rtree
+// Expired returns true if the item has expired.
+func (d *Driver) Expired() bool {
+	if d.Expiration == 0 {
+		return false
+	}
+	return time.Now().UnixNano() > d.Expiration
 }
 
 // Bounds method needs for correct working of rtree
@@ -30,50 +44,72 @@ func (d *Driver) Bounds() *rtreego.Rect {
 	return rtreego.Point{d.LastLocation.Lat, d.LastLocation.Lon}.ToRect(0.01)
 }
 
-// New creates new instance of DriverStorage
-func New() *DriverStorage {
-	d := &DriverStorage{}
-	d.drivers = make(map[int]*Driver)
-	d.locations = rtreego.NewTree(2, 25, 50)
-
-	return d
+// New initializes now storage
+func New(lruSize int) *DriverStorage {
+	s := new(DriverStorage)
+	s.drivers = make(map[int]*Driver)
+	s.locations = rtreego.NewTree(2, 25, 50)
+	s.mu = new(sync.RWMutex)
+	s.lruSize = lruSize
+	return s
 }
 
-// Set sets driver to storage by key
-func (d *DriverStorage) Set(key int, driver *Driver) {
-	_, ok := d.drivers[key]
+// Set an Driver to the storage, replacing any existing item.
+func (s *DriverStorage) Set(driver *Driver) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	d, ok := s.drivers[driver.ID]
 	if !ok {
-		d.locations.Insert(driver)
+		d = driver
+		cache, err := lru.New(s.lruSize)
+		if err != nil {
+			return errors.New("could not create LRU")
+		}
+		d.Locations = cache
+		s.locations.Insert(d)
 	}
-	d.drivers[key] = driver
+	d.LastLocation = driver.LastLocation
+	d.Locations.Add(time.Now().UnixNano(), d.LastLocation)
+	d.Expiration = driver.Expiration
+
+	s.drivers[driver.ID] = driver
+	return nil
 }
 
-// Delete removes driver from storage by key
-func (d *DriverStorage) Delete(key int) error {
-	driver, ok := d.drivers[key]
+// Delete deletes a driver from storage. Does nothing if the driver is not in the storage
+func (s *DriverStorage) Delete(id int) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	d, ok := s.drivers[id]
 	if !ok {
-		return errors.New("driver does not exist")
+		return errors.New("does not exist")
 	}
-	if d.locations.Delete(driver) {
-		delete(d.drivers, key)
+	deleted := s.locations.Delete(d)
+	if deleted {
+		delete(s.drivers, d.ID)
 		return nil
 	}
-	return errors.New("could not remove driver")
+	return errors.New("could not remove item")
 }
 
-// Get gets driver from storage and an error if nothing found
-func (d *DriverStorage) Get(key int) (*Driver, error) {
-	driver, ok := d.drivers[key]
+// Get returns driver by key
+func (s *DriverStorage) Get(id int) (*Driver, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	d, ok := s.drivers[id]
 	if !ok {
-		return nil, errors.New("driver does not exist")
+		return nil, errors.New("does not exist")
 	}
-	return driver, nil
+	return d, nil
 }
 
-// Nearest returns nearest drivers by locaion
-func (d *DriverStorage) Nearest(count int, lat, lon float64) []*Driver {
-	point := rtreego.Point{lat, lon}
-	results := d.locations.NearestNeighbors(count, point)
+// Nearest returns nearest drivers Neares
+func (s *DriverStorage) Nearest(point rtreego.Point, count int) []*Driver {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	results := s.locations.NearestNeighbors(count, point)
 	var drivers []*Driver
 	for _, item := range results {
 		if item == nil {
@@ -82,4 +118,20 @@ func (d *DriverStorage) Nearest(count int, lat, lon float64) []*Driver {
 		drivers = append(drivers, item.(*Driver))
 	}
 	return drivers
+}
+
+// DeleteExpired removes all expired items from storage
+func (s *DriverStorage) DeleteExpired() {
+	now := time.Now().UnixNano()
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	for _, v := range s.drivers {
+		if v.Expiration > 0 && now > v.Expiration {
+			deleted := s.locations.Delete(v)
+			if deleted {
+				delete(s.drivers, v.ID)
+			}
+		}
+	}
 }
